4ef8f2d2d29b1d9766bc7f25a3ead7a12766b815
Adithya <v.adithya5434@gmail.com>
2025-06-21 19:55:19 +0530

    add config

---
+++ config.py
@@ -0,0 +1,27 @@
+# INTERNAL PORTS
+# Port on which minecraft is running
+MC_SERVER_PORT = 25565
+
+# Port on which Socks 5 proxy is listening
+SOCKS5_PORT = 1080
+
+# Port on which Socks 5 proxy is listening
+SOCKS4_PORT = 1081
+
+# Port on which HTTP proxy is listening
+HTTP_PROXY_PORT = 8082
+
+# Port on which VPN  is listening
+VPN_PORT = 1194
+
+# static webpapes port: {"port":"path"} example: {"8888":"templates/html"}
+# don't use ports which are already used by other services
+STATIC_WEBPAGES_PORTS = {}
+
+
+# EXTERNAL PORTS
+# IP on which this server listens
+LISTEN_IP = "0.0.0.0"
+
+# Main port on which this server listens
+LISTEN_PORT = 8080  
