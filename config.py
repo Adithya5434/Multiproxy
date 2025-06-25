7714f4679e54054c3514a62f56f7b38274e6d94c
Adithya <v.adithya5434@gmail.com>
2025-06-25 15:00:01 +0530

    update config.py

--- config.py
+++ config.py
@@ -1,24 +1,16 @@
-# INTERNAL PORTS
+# SOCKS
+# username and password for socks5 leave empty to connect without authentication
+SOCKS5_USERNAME = ""
+SOCKS5_PASSWORD = ""
+
+# username for socks4 leave empty to connect without authentication
+SOCKS4_USERNAME = ""
+
 # Port on which minecraft is running
 MC_SERVER_PORT = 25565
 
 # Minecraft ip on which minecraft is running
 MC_SERVER_IP = "localhost"
-
-# SOCKS
-# username and password for socks5 leave empty to connect without credentials
-SOCKS5_USERNAME = ""
-SOCKS5_PASSWORD = ""
-
-# username for socks4 leave empty to connect without credentials
-SOCKS4_USERNAME = ""
-
-
-# static webpapes: 
-# structure- {"port":"path"}
-# example: {"8888":"templates/index.html", "9999":"templates/xyz.html"}
-# don't use ports which are already used by other services
-STATIC_WEBPAGES_PORTS = {}
 
 
 # EXTERNAL PORTS
